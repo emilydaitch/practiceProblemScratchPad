ae61a7cfb0ce2b34a207c0ab771828acc37a41ee
Emily <emily.daitch@gmail.com>
2020-07-25 21:41:12 -0500

    problem complete

--- cpp_challenge/sortedSquaredArray.cpp
+++ cpp_challenge/sortedSquaredArray.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <deque>
 #include <time.h>
 #include <math.h>
 
 const int MIN_VECTOR_SIZE = 0;
-const int MAX_VECTOR_SIZE = 10;
-const int MAX_ELEMENT_MAGNITUDE = 1000;
+const int MAX_VECTOR_SIZE = 10000;
+const int MAX_ELEMENT_MAGNITUDE = 10000;
 const int NUMBER_OF_RANDOM_TESTS = 10;
 
 
 void printVector(std::vector<int> toPrint) {
-	if(MAX_VECTOR_SIZE < 20) {
+	if(MAX_VECTOR_SIZE <= 20) {
 		for(int i=0; i < toPrint.size(); i++)
 		std::cout << toPrint.at(i) << ' ';
 		std::cout << std::endl;
@@ -19,7 +20,7 @@ void printVector(std::vector<int> toPrint) {
 }
 
 void printKeyAndResult(std::vector<int> key, std::vector<int> result) {
-	if(MAX_VECTOR_SIZE < 20) {
+	if(MAX_VECTOR_SIZE <= 20) {
 		std::cout << "result   : ";
 		printVector(result);
 		
@@ -29,28 +30,52 @@ void printKeyAndResult(std::vector<int> key, std::vector<int> result) {
 }
 
 std::vector<int> sortedSquaredArrayGenerator(std::vector<int> inputVector) {
-	return inputVector;
+	if(inputVector.size() == 1) {
+		inputVector.at(0) = pow(inputVector.at(0), 2);
+		return inputVector;
+	}
+	
+	std::deque<int> absoluteSortedDeque;
+	int front = 0;
+	int back = inputVector.size() - 1;
+	while(front <= back) {
+		if(abs(inputVector.at(front)) > abs(inputVector.at(back))) {
+			absoluteSortedDeque.push_front(ceil(pow(inputVector.at(front), 2)));
+			front++;
+		}
+		else {
+			absoluteSortedDeque.push_front(ceil(pow(inputVector.at(back), 2)));
+			back--;
+		}
+	}
+	
+	std::vector<int> absoluteSortedVector;
+	int saveSize = absoluteSortedDeque.size();
+	for(int i = 0; i < saveSize; ++i) {
+		absoluteSortedVector.push_back(absoluteSortedDeque.front());
+		absoluteSortedDeque.pop_front();
+	}
+	
+	return absoluteSortedVector;
 }
 
 std::vector<int> sortedSquaredArraySolutionGenerator(std::vector<int> inputVector) {
 	for(int el = 0; el < inputVector.size(); ++el) { // O(n)
 		inputVector.at(el) = pow(inputVector.at(el), 2);
 	}
-	std::cout << "squared   : ";
 	printVector(inputVector);
 	std::sort(inputVector.begin(), inputVector.end());
-	std::cout << "sorted   : ";
 	printVector(inputVector);
 	return inputVector;
 }
 
 void testSortedSquaredArray(std::vector<int> key, std::vector<int> result) {
 	if(std::equal(key.begin(), key.end(), result.begin())) {
-		std::cout << "Test passed!" << std::endl;
+		std::cout << "Test passed!" << std::endl << std::endl;
 	}
 	else {
-		std::cout << "Test failed!" << std::endl;
 		printKeyAndResult(key, result);
+		std::cout << "Test failed!" << std::endl << std::endl;
 	}
 }
 
@@ -66,8 +91,15 @@ int main() {
 
 	srand(time(0));
 	
-	std::cout << "Let's test things:" << std::endl;
-	std::cout << "Testing vector with length 0... ";
+	std::cout << "Let's test things:" << std::endl << std::endl;
+	std::cout << "Testing vector with length 0... " << std::endl;
+	
+	result = sortedSquaredArrayGenerator(emptyVec);
+	key = sortedSquaredArraySolutionGenerator(emptyVec);
+	testSortedSquaredArray(key, result);
+	
+	std::cout << "Testing vector with length 1... " << std::endl;
+	emptyVec.push_back(8);
 	
 	result = sortedSquaredArrayGenerator(emptyVec);
 	key = sortedSquaredArraySolutionGenerator(emptyVec);
@@ -85,7 +117,9 @@ int main() {
 			inputVector.push_back(randomNumber);
 		}
 		
-		std::cout << "input   : ";
+		std::sort(inputVector.begin(), inputVector.end());
+		
+		std::cout << "input  : ";
 		printVector(inputVector);
 		
 		result = sortedSquaredArrayGenerator(inputVector);

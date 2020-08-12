a6f5f7f0b00637343df9f424e29d81b89a45ca02
Emily <emily.daitch@gmail.com>
2020-08-11 22:25:19 -0500

    change timing resolution and add trials

--- cpp_challenge/radix.cpp
+++ cpp_challenge/radix.cpp
@@ -40,22 +40,55 @@ void radixSort(int arr[], int n)
 int main()
 {
 	std::cout << "Hello Radix" << std::endl;
-	int len, i;
-	len = std::pow(10,5);
-	std::cout << "Length of array: " << len << std::endl;
-	int arr[len];
-	for(i = 0; i < len; i++)
+	int i;
+	int len1 = std::pow(10,5) * .125;
+	int len2 = std::pow(10,5) * .25;
+	int len4 = std::pow(10,5) * .5;
+	int len8 = std::pow(10,5);
+	int arr1[len1];
+	int arr2[len2];
+	int arr4[len4];
+	int arr8[len8];
+	for(i = 0; i < len1; i++)
 	{
-		arr[i] = rand() % 500;
+		arr1[i] = rand() % 500;
 	}
+        for(i = 0; i < len2; i++)
+        {
+                arr2[i] = rand() % 500;
+        }
+        for(i = 0; i < len4; i++)
+        {
+                arr4[i] = rand() % 500;
+        }
+        for(i = 0; i < len8; i++)
+        {
+                arr8[i] = rand() % 500;
+        }
         
-	auto begin = std::chrono::high_resolution_clock::now();    
-    	radixSort(arr, len);
-    	auto end = std::chrono::high_resolution_clock::now();    
-    	auto dur = end - begin;
-    	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(dur).count();
+	auto begin1 = std::chrono::high_resolution_clock::now();    
+    	radixSort(arr1, len1);
+    	auto end1 = std::chrono::high_resolution_clock::now();    
+    	auto dur1 = end1 - begin1;
+        auto begin2 = std::chrono::high_resolution_clock::now();
+        radixSort(arr2, len2);
+        auto end2 = std::chrono::high_resolution_clock::now();
+        auto dur2 = end2 - begin2;
+        auto begin4 = std::chrono::high_resolution_clock::now();
+        radixSort(arr4, len4);
+        auto end4 = std::chrono::high_resolution_clock::now();
+        auto dur4 = end4 - begin4;
+        auto begin8 = std::chrono::high_resolution_clock::now();
+        radixSort(arr8, len8);
+        auto end8 = std::chrono::high_resolution_clock::now();
+        auto dur8 = end8 - begin8;
 
-	std::cout << "Radix sort took " << ms << " milliseconds to sort one million integers" << std::endl;
- 
-	
-}
+    	auto ms1 = std::chrono::duration_cast<std::chrono::microseconds>(dur1).count();
+	auto ms2 = std::chrono::duration_cast<std::chrono::microseconds>(dur2).count();
+	auto ms4 = std::chrono::duration_cast<std::chrono::microseconds>(dur4).count();
+	auto ms8 = std::chrono::duration_cast<std::chrono::microseconds>(dur8).count();
+
+	std::cout << "Radix sort took " << ms1 << " microseconds to sort 12,500 integers" << std::endl;
+        std::cout << "Radix sort took " << ms2 << " microseconds to sort 25,000 integers" << std::endl; 
+        std::cout << "Radix sort took " << ms4 << " microseconds to sort 50,000 integers" << std::endl;	
+        std::cout << "Radix sort took " << ms8 << " microseconds to sort 100,000 integers" << std::endl;}
